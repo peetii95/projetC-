54a2e064d2aed13e089c65efe457463eb7ff2fb5
PhO <hotom95@gmail.com>
2017-03-17 10:32:36 +0100

    U s*ck af...

---
+++ EtienneTheSecond.cpp
@@ -0,0 +1,21 @@
+/*
+ * EtienneTheSecond.cpp
+ *
+ *  Created on: 17 mars 2017
+ *      Author: emacedegastines
+ */
+
+#include "EtienneTheSecond.h"
+
+namespace std {
+
+EtienneTheSecond::EtienneTheSecond() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EtienneTheSecond::~EtienneTheSecond() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ EtienneTheSecond.h
@@ -0,0 +1,21 @@
+/*
+ * EtienneTheSecond.h
+ *
+ *  Created on: 17 mars 2017
+ *      Author: emacedegastines
+ */
+
+#ifndef ETIENNETHESECOND_H_
+#define ETIENNETHESECOND_H_
+
+namespace std {
+
+class EtienneTheSecond {
+public:
+	EtienneTheSecond();
+	virtual ~EtienneTheSecond();
+};
+
+} /* namespace std */
+
+#endif /* ETIENNETHESECOND_H_ */
